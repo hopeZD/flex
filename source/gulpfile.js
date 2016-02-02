526facefa924681bbd9a8e5390f04299d787e677
zhengzuodi <zhengzuodi@gmail.com>
2016-02-03 03:47:35 +0800

    complete

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,17 +1,48 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var wrap = require('gulp-wrap');
+var browserSync = require('browser-sync');
+
+
+
+function handleError(err) {
+	console.log(err.toString());
+	this.emit('end');
+}
+
+gulp.task('browser-sync', ['build', 'sass'], function(){
+	browserSync({
+		server: {
+			baseDir: '..'
+		}
+	});
+});
+
+gulp.task('build', function(){
+  gulp.src("pages/*.html")
+      .pipe(wrap({src:"layout/default.html"}))
+      .pipe(gulp.dest(".."));
+});
+
 
 gulp.task('sass', function(){
-  gulp.src('styles/main.scss')
-      .pipe(sass())
-      .pipe(prefix())
-      .pipe(gulp.dest('../styles'));
+	gulp.src('styles/main.scss')
+		.pipe(sass().on('error', handleError))
+		.pipe(prefix())
+		.pipe(gulp.dest('../styles'))
+		.pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('cp', function() {
-	gulp.src('index.html')
-	 		.pipe(gulp.dest('..'));
+gulp.task('rebuild', ['build'], function(){
+	browserSync.reload();
 });
 
-gulp.task('default', ['sass', 'cp']);
+
+gulp.task('watch', function() {
+	gulp.watch(['**/*.html'], ['rebuild']);
+	gulp.watch(['styles/*.scss'], ['sass'])
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
+
